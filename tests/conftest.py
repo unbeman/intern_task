9f3d0f813e4435e3cc62e33407b4c80ee9703c0a
Ann <unbeman@gmail.com>
2019-03-29 02:32:51 +0300

    edit conftest, add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,59 @@
+import asyncio
+
 import pytest
+
+from initial import get_config
 from source.app import init_app
+from source.database_connector.postgres_connector import AsyncPostgresqlConnector
+from db_helpers import setup_db, teardown_db, drop_tables, create_tables, create_sample_data
 
 test_config = '../example_config.ini'
 
+@pytest.yield_fixture(scope='session')
+def event_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(None)
+
+    yield loop
+
+    if not loop.is_closed():
+        loop.call_soon(loop.stop)
+        loop.run_forever()
+        loop.close()
+
+
+@pytest.fixture(scope='session')
+def loop(event_loop):
+    return event_loop
+
 @pytest.fixture
-def api(loop, test_client):
+async def api(aiohttp_client):
     app = init_app(['-c', test_config])
-    yield loop.run_until_complete(test_client(app))
-    loop.run_until_complete(app.shutdown())
+    return await aiohttp_client(app)
+
+
+@pytest.fixture(scope='session')
+async def db_connector(loop):
+    db_settings = get_config(['-c', test_config])['database_settings']
+    db_conn = AsyncPostgresqlConnector(**db_settings)
+    await db_conn.init_admin_db()
+    return db_conn
+
+
+@pytest.fixture(scope='session')
+async def database(db_connector, loop):
+    await setup_db(db_connector)
+    yield
+    await teardown_db(db_connector)
+
+
+@pytest.fixture
+async def tables_and_data(db_connector, database):
+    print("DB NAME", db_connector.db_name)
+    await db_connector.init()
+    await create_tables(db_connector)
+    await create_sample_data(db_connector)
+
+    yield
+
+    await drop_tables(db_connector)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,10 +1,21 @@
-async def test_get_company(api):
-    resp = await api.get('/company/1')
-    assert resp.status == 200
-    text = await resp.text()
+async def test_get_company(tables_and_data, api):
+    expected_data = {"title": "Plushki", "description": "Bread and cakes", "id": 1}
+    response = await api.get('/company/1')
+    assert response.status == 200
+    data = await response.json()
+    assert data == expected_data
 
 
-async def test_get_worker(api):
-    resp = await api.get('/worker/1')
-    assert resp.status == 200
-    text = await resp.text()
+async def test_get_nonexistent_company(tables_and_data, api):
+    response = await api.get('/company/2')
+    assert response.status == 404
+
+
+async def test_get_worker(tables_and_data, api):
+    expected_data = {"full_name": "Vasya Pupkin", "position": "seller",
+                     "phone_number": "89998765432",
+                     "company_id": 1, "id": 1}
+    response = await api.get('/worker/1')
+    assert response.status == 200
+    data = await response.json()
+    assert data == expected_data
